8a02eda2f6e618781bb59676ab9519c8d6836af5
amiblog <reza.am.0425@gmail.com>
2022-04-05 08:27:25 +0000

    bootstrap導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     post_image=PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
-    redirect_to post_image_path(post_image)
+    redirect_to post_image_path(post_image.id)
   end
 
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
-    redirect_to post_image_path(post_image)
+    redirect_to post_image_path(post_image.id)
   end
 end
